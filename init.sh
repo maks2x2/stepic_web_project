bb676ad360be37bad9937c8242b1b985cb8e7fd6
maks2x2 <maks2x2@mail.ru>
2016-05-09 21:12:44 +0700

    2

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo /bin/﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo chmod -R 777 /home/box/web/* 
-sudo /etc/init.d/nginx restart
+rm /etc/nginx/sites-enabled/test.conf
+﻿#ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+rm /etc/nginx/sites-enabled/default
+chmod -R 777 /home/box/web/* 
+/etc/init.d/nginx restart
