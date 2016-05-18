e9ff181f00d6031f7736fd543fb0afe59e3c578c
maks2x2 <maks2x2@mail.ru>
2016-05-19 00:03:08 +0700

    fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from cgi import parse_qs
+#from cgi import parse_qs
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plan')])
-    qs=parse_qs(env['QUERY_STRING'])
-    #return ['Hello World!']
-    res = ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
-    #print (res)
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    #qs=parse_qs(env['QUERY_STRING'])
+    res = env['QUERY_STRING'].split('&')
+    #res = ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
+    res = [item+'\r\n' for item in res]
     return res
